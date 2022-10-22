9167e3bab443b7dd3bc1200295c5fbc7ea377008
Philani Ncube <ncbphi001@gmail.com>
2022-10-22 18:44:38 +0200

    bundle products layout

--- pages/admin/bundles/[id].tsx
+++ pages/admin/bundles/[id].tsx
@@ -21,13 +21,8 @@ const Product = ({
   products: IProduct[];
   bundleProducts:IBundleProduct[];
 }) => {
-  console.log({ bundle, bundleProducts });
-
-  console.log('price',bundleProducts.reduce((acc, product) => acc + product.quantity * product.product_id.price, 0))
 
   const router = useRouter()
-
-
 
   const [uploadData, setUploadData] = useState({});
 
@@ -205,38 +200,6 @@ const Product = ({
                 {bundle.description}
               </p>
             </div>
-            <div className="mt-3">
-              <h2 className="my-2">Products In Bundle</h2>
-              {bundleProducts.map((product) => (
-                <div
-                  key={product.product_id.id}
-                  className="text-sm p-3 flex items-center space-x-3 my-1 rounded-lg bg-gray-300"
-                >
-                  <div className="text-bold flex flex-1 flex-col space-y-2">
-                    <p>{product.product_id.name}</p>
-                    <p>
-                      Price: {formatCurrency(product.product_id.price)} &times;
-                      {product.quantity}
-                    </p>
-                  </div>
-                  <svg
-                    onClick={() => deleteFromBundle(product.product_id.id)}
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-10 h-10 cursor-pointer text-red-600"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
-                </div>
-              ))}
-            </div>
           </div>
 
           <Image
@@ -247,8 +210,43 @@ const Product = ({
             className="w-60 h-60 object-cover"
           />
         </div>
+        <div className="mt-3">
+          <h2 className="my-2 font-extrabold text-2xl text-gray-500">Products In Bundle</h2>
+          <div className="grid grid-cols-3 gap-2 mb-4">
+            {bundleProducts.map((product) => (
+              <div
+                key={product.product_id.id}
+                className="text-sm p-3 flex items-center space-x-3 my-1 rounded-lg bg-gray-300"
+              >
+                <div className="text-bold flex flex-1 flex-col space-y-2">
+                  <p>{product.product_id.name}</p>
+                  <p>
+                    Price: {formatCurrency(product.product_id.price)} &times;
+                    {product.quantity}
+                  </p>
+                </div>
+                <svg
+                  onClick={() => deleteFromBundle(product.product_id.id)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-10 h-10 cursor-pointer text-red-600"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </div>
+            ))}
+          </div>
+        </div>
+
         <div>
-          <h2>Add Bundle Product</h2>
+          <h2>Add Product To Bundle</h2>
           <div className="p-8 my-4 border-spacing-3 border border-dashed rounded-lg border-slate-500">
             <h3 className="font-bold text-xl text-primary-main my-1">
               Select products to add to bundle
