a9f64b627533b3c2744c75620b84623ea6bdec18
Philani Ncube <ncbphi001@gmail.com>
2023-02-10 18:50:32 +0200

    bundles table

---
+++ components/Admin/Bundles/BundlesTable.tsx
@@ -0,0 +1,134 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { format } from "date-fns";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { Database } from "../../../db_types";
+import formatCurrency from "../../../lib/formatCurrency";
+
+type ComponentProps = {
+  bundles : IBundle[]
+}
+
+function BundlesTable({bundles}:ComponentProps) {
+  const [show, setShow] = useState<null|number>(null);
+
+  const router = useRouter()
+
+    const supabase = useSupabaseClient<Database>();
+
+    const createProduct = async () => {
+      const { data, error } = await supabase
+        .from("bundles")
+        .insert([
+          {
+            title: "Draft Bundle",
+            slug: Math.random().toString(),
+            description: "",
+          },
+        ])
+        .select("*")
+        .single();
+
+      if (error) {
+        alert(error.details);
+      } else if (data) {
+        router.push(`/admin/bundles/${data.id}`);
+      }
+    };
+
+  return (
+    <>
+      <div className="w-full sm:px-6">
+        <div className="px-4 md:px-10 py-4 md:py-7 bg-gray-100 rounded-tl-lg rounded-tr-lg">
+          <div className="sm:flex items-center justify-between">
+            <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">
+              Bundles
+            </p>
+            <div>
+              <button className="inline-flex sm:ml-3 mt-4 sm:mt-0 items-start justify-start px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded">
+                <p
+                  onClick={createProduct}
+                  className="text-sm font-medium leading-none text-white"
+                >
+                  New Bundle
+                </p>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto">
+          <table className="w-full whitespace-nowrap">
+            <thead>
+              <tr className="h-16 w-full text-sm leading-none text-gray-800">
+                <th className="font-normal text-left pl-4">Bundle</th>
+                <th className="font-normal text-left pl-12">Featured</th>
+                <th className="font-normal text-left pl-12">Created</th>
+                <th className="font-normal text-left pl-20">Pricing</th>
+                <th className="font-normal text-left pl-20">Gender</th>
+                <th className="font-normal text-left pl-16">Category</th>
+              </tr>
+            </thead>
+            <tbody className="w-full">
+              {bundles.map((bundle) => (
+                <tr
+                  key={bundle.id}
+                  onClick={() => router.push(`/admin/bundles/${bundle.id}`)}
+                  className="h-20 text-sm leading-none text-gray-800 bg-white hover:bg-gray-100 cursor-pointer border-b border-t border-gray-100"
+                >
+                  <td className="pl-4 cursor-pointer">
+                    <div className="flex items-center">
+                      <div className="w-10 h-10">
+                        <img
+                          className="w-full h-full"
+                          src={bundle.main_image.url}
+                        />
+                      </div>
+                      <div className="pl-4">
+                        <p className="font-medium">{bundle.title}</p>
+                        <p className="text-xs leading-3 text-gray-600 pt-2"></p>
+                      </div>
+                    </div>
+                  </td>
+                  <td className="pl-12">
+                    <p className="text-sm font-medium leading-none text-gray-800">
+                      {bundle.featured ? "Featured" : "Not Featured"}
+                    </p>
+                  </td>
+                  <td className="pl-12">
+                    <p className="font-medium">
+                      {format(new Date(bundle.created_at), "yyyy-MM-dd")}
+                    </p>
+                  </td>
+                  <td className="pl-20">
+                    <p className="font-medium text-green-600">
+                      {" "}
+                      {formatCurrency(bundle.price)}
+                    </p>
+                    <p className="text-xs leading-3 text-red-600 mt-2">
+                      {formatCurrency(bundle.cost)}
+                    </p>
+                  </td>
+                  <td className="pl-20">
+                    <p className="font-medium">
+                      {bundle.gender === "him" ? "For Him" : "For Her"}
+                    </p>
+                  </td>
+                  <td className="pl-16">
+                    <div className="flex items-center">
+                      <p className="font-medium">
+                        {bundle.category.name}
+                      </p>
+                    </div>
+                  </td>
+
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BundlesTable;
--- pages/admin/bundles/index.tsx
+++ pages/admin/bundles/index.tsx
@@ -1,7 +1,9 @@
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import BundlesTable from "../../../components/Admin/Bundles/BundlesTable";
 import Layout from "../../../components/Admin/Layout";
 import { Database } from "../../../db_types";
 import { getBundles } from "../../../fetchers/bundles";
@@ -13,84 +15,20 @@ const Products = ({ bundles }: { bundles: IBundle[] }) => {
 
       const router = useRouter();
 
-      const supabase = useSupabaseClient<Database>()
 
-      const createProduct = async () => {
-        const { data, error } = await supabase
-          .from("bundles")
-          .insert([
-            {
-              title: "Draft Bundle",
-              slug: Math.random().toString(),
-              description: "",
-
-            },
-          ])
-          .select("*")
-          .single();
-
-        if (error) {
-          alert(error.details);
-        } else if (data) {
-          router.push(`/admin/bundles/${data.id}`);
-        }
-      };
 
 
   return (
     <Layout>
+      <Head>
+        <title>Bundles | Admin | All Things Gifts</title>
+      </Head>
       <div>
-        <div className="flex justify-between items-center w-full my-3">
-          <Link
-            href="/admin/dashboard"
-            className="font-bold text-primary-main text-2xl"
-          >
-            Back To Dashboard
-          </Link>
 
-          <button
-            onClick={createProduct}
-            className="text-white bg-primary-main font-bold text-lg uppercase px-8 py-2 rounded-lg"
-          >
-            New Bundle
-          </button>
-        </div>
 
-        <div className="w-full grid grid-cols-2 gap-6">
-          {bundles?.map((bundle) => (
-            <Link key={bundle.id} href={`/admin/bundles/${bundle.id}`}>
-              <div className="flex gap-3 hover:bg-gray-200 py-4 px-3 cursor-pointer">
-                <Image
-                  src={bundle.main_image.url}
-                  width={100}
-                  height={100}
-                  alt={bundle.main_image.original_filename}
-                  className="h-full aspect-square object-cover rounded"
-                />
-                <div className="">
-                  <h3 className="text-primary-main font-bold">
-                    {bundle.title}
-                  </h3>
-                  <div className="w-full flex justify-between font-medium text-md py-2">
-                    <p>Price: {formatCurrency(bundle.price)}</p>
-                    <p>Cost: {formatCurrency(bundle.cost)}</p>
-                  </div>
-                  <p className="text-xs text-slate-600 font-medium">
-                    {bundle.description}
-                  </p>
-                  <div className="flex justify-between items-center">
-                    <h4 className="text-md mt-2 text-primary-main font-bold">
-                      {bundle.category?.name}
-                    </h4>
-                    <h4 className="text-md capitalize mt-2 text-primary-main font-bold">
-                      {bundle.gender}
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
+        <BundlesTable bundles={bundles} />
+
+
       </div>
     </Layout>
   );
@@ -99,7 +37,7 @@ const Products = ({ bundles }: { bundles: IBundle[] }) => {
 export default Products;
 
 export async function getServerSideProps() {
-  const bundles = (await getBundles()) as IBundle[];
+  const bundles = await getBundles() as IBundle[];
 
   return {
     props: {
