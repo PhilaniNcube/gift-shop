b12dbf3825aaa27f577c11593687856a41dc8c84
Philani Ncube <ncbphi001@gmail.com>
2023-02-19 20:25:45 +0200

    cross-origin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="use-credentials"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap"
